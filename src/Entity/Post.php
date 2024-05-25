37052d323fda27fcf57a12980e09b641b52a421a
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-25 17:33:45 +0200

    refactor from fork

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,127 +4,128 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\PostRepository;
+use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ApiResource()]
+#[ApiResource]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $title = null;
+  #[ORM\Column(length: 255)]
+  private ?string $title = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $content = null;
+  #[ORM\Column(type: Types::TEXT)]
+  private ?string $content = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+  #[ORM\Column]
+  private ?DateTimeImmutable $createdAt = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $updatedAt = null;
+  #[ORM\Column]
+  private ?DateTimeImmutable $updatedAt = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $slug = null;
+  #[ORM\Column(length: 255)]
+  private ?string $slug = null;
 
-    #[ORM\ManyToOne(inversedBy: 'posts')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Category $category = null;
+  #[ORM\ManyToOne(inversedBy: 'posts')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?Category $category = null;
 
-    #[ORM\ManyToOne(inversedBy: 'posts')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $author = null;
+  #[ORM\ManyToOne(inversedBy: 'posts')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?User $author = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
 
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
+  public function setTitle(string $title): static
+  {
+    $this->title = $title;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
+  public function getContent(): ?string
+  {
+    return $this->content;
+  }
 
-    public function setContent(string $content): static
-    {
-        $this->content = $content;
+  public function setContent(string $content): static
+  {
+    $this->content = $content;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
+  public function getCreatedAt(): ?DateTimeImmutable
+  {
+    return $this->createdAt;
+  }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
-    {
-        $this->createdAt = $createdAt;
+  public function setCreatedAt(DateTimeImmutable $createdAt): static
+  {
+    $this->createdAt = $createdAt;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getUpdatedAt(): ?\DateTimeImmutable
-    {
-        return $this->updatedAt;
-    }
+  public function getUpdatedAt(): ?DateTimeImmutable
+  {
+    return $this->updatedAt;
+  }
 
-    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
-    {
-        $this->updatedAt = $updatedAt;
+  public function setUpdatedAt(DateTimeImmutable $updatedAt): static
+  {
+    $this->updatedAt = $updatedAt;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
+  public function getSlug(): ?string
+  {
+    return $this->slug;
+  }
 
-    public function setSlug(string $slug): static
-    {
-        $this->slug = $slug;
+  public function setSlug(string $slug): static
+  {
+    $this->slug = $slug;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
+  public function getCategory(): ?Category
+  {
+    return $this->category;
+  }
 
-    public function setCategory(?Category $category): static
-    {
-        $this->category = $category;
+  public function setCategory(?Category $category): static
+  {
+    $this->category = $category;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getAuthor(): ?User
-    {
-        return $this->author;
-    }
+  public function getAuthor(): ?User
+  {
+    return $this->author;
+  }
 
-    public function setAuthor(?User $author): static
-    {
-        $this->author = $author;
+  public function setAuthor(?User $author): static
+  {
+    $this->author = $author;
 
-        return $this;
-    }
+    return $this;
+  }
 }
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -2,6 +2,7 @@
 
 namespace App\Security;
 
+use Exception;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,44 +19,45 @@ use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class UserAuthenticator extends AbstractLoginFormAuthenticator
 {
-    use TargetPathTrait;
+  use TargetPathTrait;
 
-    public const LOGIN_ROUTE = 'app_login';
+  public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
+  public function __construct(private readonly UrlGeneratorInterface $urlGenerator)
+  {
+  }
+
+  public function authenticate(Request $request): Passport
+  {
+    $email = $request->request->get('email', '');
+
+    $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+
+    return new Passport(
+      new UserBadge($email),
+      new PasswordCredentials($request->request->get('password', '')),
+      [
+        new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+        new RememberMeBadge(),
+      ]
+    );
+  }
+
+  /**
+   * @throws Exception
+   */
+  public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+  {
+    if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+      return new RedirectResponse($targetPath);
     }
 
-    public function authenticate(Request $request): Passport
-    {
-        $email = $request->request->get('email', '');
+    return new RedirectResponse($this->urlGenerator->generate('account'));
+    throw new Exception('TODO: provide a valid redirect inside ' . __FILE__);
+  }
 
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
-
-        return new Passport(
-            new UserBadge($email),
-            new PasswordCredentials($request->request->get('password', '')),
-            [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
-                new RememberMeBadge(),
-            ]
-        );
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
-
-        return new RedirectResponse($this->urlGenerator->generate('account'));
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-    }
-
-    protected function getLoginUrl(Request $request): string
-    {
-        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
-    }
+  protected function getLoginUrl(Request $request): string
+  {
+    return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+  }
 }
