4364680efb9988d01ca8ec5e337673682980f871
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-26 10:13:44 +0200

    feat: use content form another author

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -21,16 +21,12 @@ class RegistrationController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-      // Set username as email if needed
-      // $user->setUsername($user->getEmail());
-
       // Encode the plain password
-      $user->setPassword(
-        $userPasswordHasher->hashPassword(
-          $user,
-          $form->get('plainPassword')->getData()
-        )
+      $hashedPassword = $userPasswordHasher->hashPassword(
+        $user,
+        $form->get('plainPassword')->getData()
       );
+      $user->setPassword($hashedPassword);
 
       $entityManager->persist($user);
       $entityManager->flush();
