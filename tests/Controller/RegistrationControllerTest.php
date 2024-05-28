3e4bbc25b7dede22ff0bb21af60487a13f28c6b7
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-05-28 14:43:36 +0200

    test redirection

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Controller;
 
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationControllerTest extends WebTestCase
@@ -16,9 +15,6 @@ class RegistrationControllerTest extends WebTestCase
     $this->assertSelectorTextContains('h1', 'Register');
   }
 
-  /**
-   * @throws Exception
-   */
   public function testRegisterFormSubmission()
   {
     $client = static::createClient();
@@ -39,14 +35,17 @@ class RegistrationControllerTest extends WebTestCase
     // Suivre la redirection
     $client->followRedirect();
 
-    // Vérifiez que la soumission du formulaire est réussie et que l'utilisateur est redirigé
-    try {
-      $this->assertResponseIsSuccessful();
-      // Vérifiez si la réponse contient un message de succès ou redirige vers la page de connexion
-      $this->assertSelectorExists('.alert-danger');
-    } catch (Exception $e) {
-      echo $client->getResponse()->getContent();
-      throw $e;
-    }
+    // Vérifiez que la réponse après redirection est réussie (200)
+    $this->assertResponseIsSuccessful();
+
+    // Optionnel : Vérifiez que l'utilisateur est redirigé vers la page d'accueil
+    $this->assertPageTitleSame('Home');
+    $this->assertSelectorTextContains('h1', 'Welcome to the Home Page!');
+
+    // Optionnel : Afficher le contenu pour le débogage si nécessaire
+    echo $client->getResponse()->getContent();
+
+    // Vérifiez l'absence de message d'erreur
+    $this->assertSelectorNotExists('.alert-danger');
   }
 }
