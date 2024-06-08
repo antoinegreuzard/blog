a9b43b5e8ca6fe00577566182139040a9ed23f4c
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-06-08 21:04:56 +0200

    Update editor settings and refactor SecurityControllerTest
    The .fleet/settings.json file has been modified to include new editor guides. In SecurityControllerTest.php, multiline comments and function calls have been reformatted to improve readability. This refactoring enhances consistency and complies with the best coding practices.

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -8,21 +8,30 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
- * Class SecurityControllerTest extends the WebTestCase to create a series of tests related to application's security routes.
+ * Class SecurityControllerTest extends the WebTestCase to create a series of
+ * tests related to application's security routes.
  * This class has following five methods:
  *
- * - The `testLoginPageLoads`: This method ensures that the login page is being loaded successfully.
- * - The `testLoginFormSubmission`: It tests the submission of the login form with valid user credentials.
- * - The `testLoginFormSubmissionWithInvalidCredentials`: It tests the submission of login form with invalid credentials,
+ * - The `testLoginPageLoads`: This method ensures that the login page is being
+ * loaded successfully.
+ * - The `testLoginFormSubmission`: It tests the submission of the login form
+ * with valid user credentials.
+ * - The `testLoginFormSubmissionWithInvalidCredentials`: It tests the
+ * submission of login form with invalid credentials,
  * ensuring only valid users can process through login page.
- * - The `testLogout`: It tests the logout functionality, ensuring the logout action leads to the correct endpoint.
+ * - The `testLogout`: It tests the logout functionality, ensuring the logout
+ * action leads to the correct endpoint.
  *
- * Each test case follows a similar pattern where it starts by creating a client to make HTTP requests to application's routes.
- * After a request has been sent, it uses built-in assert methods provided by WebTestCase to confirm that the received response
+ * Each test case follows a similar pattern where it starts by creating a client
+ * to make HTTP requests to application's routes.
+ * After a request has been sent, it uses built-in assert methods provided by
+ * WebTestCase to confirm that the received response
  * meets the expected criteria.
  *
- * The `SecurityControllerTest` class ensures the integrity of the application's login system by simulating the web interaction a
- * user would have when entering, submitting or failing to meet the criteria of the login form, and finally logging out.
+ * The `SecurityControllerTest` class ensures the integrity of the application's
+ * login system by simulating the web interaction a
+ * user would have when entering, submitting or failing to meet the criteria of
+ * the login form, and finally logging out.
  */
 class SecurityControllerTest extends WebTestCase
 {
@@ -57,7 +66,9 @@ class SecurityControllerTest extends WebTestCase
             $testUser = new User();
             $testUser->setEmail('test@example.com');
             $testUser->setUsername('test');
-            $testUser->setPassword(password_hash('password123', PASSWORD_BCRYPT));
+            $testUser->setPassword(
+                password_hash('password123', PASSWORD_BCRYPT)
+            );
 
             $entityManager->persist($testUser);
             $entityManager->flush();
@@ -100,7 +111,10 @@ class SecurityControllerTest extends WebTestCase
         // Follow the redirect to see the error message
         $client->followRedirect();
         $this->assertSelectorExists('.alert-danger');
-        $this->assertSelectorTextContains('.alert-danger', 'Invalid credentials.');
+        $this->assertSelectorTextContains(
+            '.alert-danger',
+            'Invalid credentials.'
+        );
     }
 
     /**
